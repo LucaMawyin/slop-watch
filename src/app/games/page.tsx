cdc23b36ea128807534e0aad1c41d2e69fd710ae
LucaMawyin <lucamawyin@gmail.com>
2026-08-15 00:22:05 -0400

    testing api url

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -20,7 +20,7 @@ function GamesContent() {
     useEffect(() => {
         setLoading(true);
 
-        fetch(`https://api.lucamawyin.com/slop/api/games?league=${league}`)
+        fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/games?league=${league}`)
             .then((res) => {
                 console.log(res)
                 if (!res.ok) {
