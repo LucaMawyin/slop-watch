8faf83efc068c0843a9b9e410cad486df777b605
lucamawyin <lucamawyin@gmail.com>
2026-08-16 17:16:25 +0100

    python script for updating csv data

---
+++ python/services/test.py
@@ -0,0 +1,6 @@
+from services.update_data import update_data
+
+update_data("nba")
+update_data("nhl")
+update_data("nfl")
+update_data("mlb")
---
+++ python/services/update_data.py
@@ -0,0 +1,42 @@
+from config.sports import SPORT_CONFIG
+from pathlib import Path
+import pandas as pd
+from datetime import date
+
+def update_data(league="nba"):
+
+    # Initial data
+    config = SPORT_CONFIG[league]
+    output_path = Path(config["output"])
+    df = pd.read_csv(output_path)
+
+    # Getting today's games
+    today = date.today()
+    date_string = today.strftime("%Y%m%d")
+
+    new_games = config["schedule_function"](
+        dates=date_string,
+        return_as_pandas=True,
+        limit=50
+    )
+
+    # Only modify csv if we get new games
+    if new_games is not None and not new_games.empty:
+
+        # Removing oldest date
+        df["_date"] = pd.to_datetime(df["date"]).dt.date
+        oldest_date = df["_date"].min()
+        df = df[df["_date"] != oldest_date]
+        df = df.drop(columns="_date")
+
+        df = pd.concat([df, new_games], ignore_index=True)
+
+        df.to_csv(output_path,index=False)
+
+        print(
+            f"{league.upper()}: removed {oldest_date}, "
+            f"added {len(new_games)} games for {today}"
+        )
+
+    else:
+        print(f"{league.upper()}: no games found for {today}")
